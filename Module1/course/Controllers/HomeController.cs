79bab8c419e9150e475916d1045ea379888f8c60
Usov <usov.lesha2017@gmail.com>
2024-03-08 13:49:00 +0300

    start

---
+++ Module1/course/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using course.Models;
+
+namespace course.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly IConfiguration _configuration;
+    public HomeController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public IActionResult Index()
+    {
+        var admin = _configuration.GetSection("Admin");
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Module1/course/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace course.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
