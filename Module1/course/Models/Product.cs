9952444c63f5e78d4bc7c491c706e40b3bba5563
Usov <usov.lesha2017@gmail.com>
2024-03-08 13:47:14 +0300

    Start coure

---
+++ Module1/course/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+
+namespace course.Models;
+
+public class Product
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Country { get; set; }
+    public int[] Ratings { get; set; }
+    public override string ToString() => JsonSerializer.Serialize<Product>(this);
+    
+}
