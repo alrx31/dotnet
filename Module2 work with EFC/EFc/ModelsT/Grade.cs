ad25b51fadab210db4949d59220f3e0486c3b36e
Usov <usov.lesha2017@gmail.com>
2024-03-09 12:42:01 +0300

    Create Data annotation using atributes

--- Module2 work with EFC/EFc/ModelsT/Grade.cs
+++ Module2 work with EFC/EFc/ModelsT/Grade.cs
@@ -1,13 +1,22 @@
-﻿namespace EFc.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace EFc.Models;
+
+[Table("Grades")]
 public class Grade
 {
     public Grade()
     {
         Students = new List<Student>();
     }
+    
+    [Key]
     public int GradeId { get; set; }
+    [Column("GradeName", TypeName = "ntext")]
+    [MaxLength(50)]
     public string GradeName { get; set; }
+    
     public IList<Student> Students { get; set; }
 
 }
--- Module2 work with EFC/EFc/ModelsT/Student.cs
+++ Module2 work with EFC/EFc/ModelsT/Student.cs
@@ -1,15 +1,34 @@
-﻿namespace EFc.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace EFc.Models;
+
+[Table("Students")]
 public class Student
 {
+    [Column("StudentId")]
+    [Key]
     public int StudentId { get; set; }
+    [Column("FirstName", TypeName = "ntext")]
+    [MaxLength(50)]
+    
     public string FirstName { get; set; }
+    [Column("LastName", TypeName = "ntext")]
+    [MaxLength(50)]
     public string LastName { get; set; }
+    [Column("DateOfBirth", TypeName = "DATE")]
+    
     public DateTime DateOfBirth { get; set; }
+    [Column("Photo", TypeName = "image")]
+    
     public byte[] Photo { get; set; }
+    [Column("Height", TypeName = "decimal(5, 2)")]
     public decimal Height { get; set; }
+    [Column("Weight", TypeName = "decimal(5, 2)")]
     public float Weight { get; set; }
-
+    [Column("GradeId")]
+    [ForeignKey("GrdId")]
     public int GradeId { get; set; }
+    
     public Grade Grade { get; set; }
 }
